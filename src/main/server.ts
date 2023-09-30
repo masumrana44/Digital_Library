9a8d5537d876145cd7860c5417e48a3150f337ba
Masum Rana <masumrana446267@gmail.com>
2023-09-30 19:51:38 +0600

    professtional express mongoose project setup done with eslint ,prettier, husky and lint-staged

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,17 +1,17 @@
-import mongoose from "mongoose";
-import config from "../config";
-import app from "./app";
+import mongoose from 'mongoose'
+import config from '../config'
+import app from './app'
 
 const run = () => {
   try {
-    mongoose.connect(config.database_uri as string);
-    console.log("Database connected");
+    mongoose.connect(config.database_uri as string)
+    console.log('Database connected')
     app.listen(config.port, () => {
-      console.log(`Server running on port ${config.port}`);
-    });
+      console.log(`Server running on port ${config.port}`)
+    })
   } catch (error) {
-    console.log(`Failed to connet Database`, error);
+    console.log(`Failed to connet Database`, error)
   }
-};
+}
 
-run();
+run()
